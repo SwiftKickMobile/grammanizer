05fdd9b9cb359d4a7bd31fe6bcbac18f02601389
Travis Buttaccio <travis@swiftkick.it>
2018-07-13 10:55:08 -0500

    Initial Commit

---
+++ Grammanizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grammanizer
+//
+//  Created by Travis Buttaccio on 7/13/18.
+//  Copyright © 2018 SwiftKick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
